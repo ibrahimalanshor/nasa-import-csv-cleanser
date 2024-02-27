c6b75bfa035d8dd9f20f53668d51ceb199dfa106
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-28 06:12:38 +0700

    bank

--- 2024/main.py
+++ 2024/main.py
@@ -9,32 +9,70 @@ mongo_uri = "mongodb+srv://ibrahimalanshor:65cZx8wS7s0kvjrt@cluster0.bw1af.mongo
 mongo_client = MongoClient(mongo_uri, server_api=ServerApi('1'))
 db = mongo_client.nasa_import
 
-filepath = os.path.join(os.path.dirname(__file__), './files/stockist-penjualan.csv')
-chunks = pd.read_csv(filepath, chunksize=1000)
-filesize = len(pd.read_csv(filepath).index)
+def import_stockist_penjualan():
+    print('Importing raw stockist penjualan')
 
-print('Importing raw stockist penjualan')
-with progressbar.ProgressBar(max_value=filesize) as bar:
-    for df in chunks:
-        payload = [
-            UpdateOne(
-                {'code': row['KDST']},
-                {'$set': {
-                    'code': row['KDST'],
-                    'name': row['NMAST'],
-                    'address': row['ALMST'],
-                    'address2': row['ALMST1'],
-                    'phone': row['TLPST'],
-                    'area': row['AREA'],
-                    'order': row['URUT'],
-                    'is_active': 1 if row['PASIF'] == 'x' else 0,
-                    'email': row['EMAIL'] if type(row['EMAIL']) is str else f'{row["KDST"].lower()}@naturalnusantara.co.id'
-                }},
-                upsert=True
-            )
-            for _, row in df.iterrows()
-        ]
+    filepath = os.path.join(os.path.dirname(__file__), './files/stockist-penjualan.csv')
+    chunks = pd.read_csv(filepath, chunksize=1000, encoding='latin1', keep_default_na=False)
+    filesize = len(pd.read_csv(filepath, encoding='latin1').index)
 
-        db.stockists.bulk_write(payload)
+    with progressbar.ProgressBar(max_value=filesize) as bar:
+        for df in chunks:
+            payload = [
+                UpdateOne(
+                    {'code': row['KDST']},
+                    {'$set': {
+                        'code': row['KDST'],
+                        'name': row['NMAST'],
+                        'address': row['ALMST'],
+                        'address2': row['ALMST1'],
+                        'phone': row['TLPST'],
+                        'area': row['AREA'],
+                        'order': row['URUT'],
+                        'is_active': 1 if row['PASIF'] == 'x' else 0,
+                        'email': row['EMAIL'] if row['EMAIL'] else f'{row["KDST"].lower()}@naturalnusantara.co.id'
+                    }},
+                    upsert=True
+                )
+                for _, row in df.iterrows()
+            ]
 
-        bar.update(1000)
+            db.stockists.bulk_write(payload)
+
+            bar.update(1000)
+
+def import_stockist_keuangan():
+    print('Importing raw stockist penjualan')
+
+    filepath = os.path.join(os.path.dirname(__file__), './files/stockist-keuangan.csv')
+    chunks = pd.read_csv(filepath, chunksize=1000, encoding='latin1', keep_default_na=False)
+    filesize = len(pd.read_csv(filepath, encoding='latin1').index)
+
+    with progressbar.ProgressBar(max_value=filesize) as bar:
+        for df in chunks:
+            payload = [
+                UpdateOne(
+                    {'code': row['KODEST']},
+                    {'$set': {
+                        'code': row['KODEST'],
+                        'name': row['NMAST'],
+                        'bank_name': row['KODEBANK'],
+                        'bank_branch_name': row['CABANG'],
+                        'bank_account_name': row['NMABANK'],
+                        'bank_account_number': int(row['NOREK']) if type(row['NOREK']) is int else row['NOREK'],
+                        'order': row['URUT'],
+                    }},
+                    upsert=True
+                )
+                for _, row in df.iterrows()
+            ]
+
+            db.stockists.bulk_write(payload)
+
+            bar.update(1000)
+
+
+import_stockist_penjualan()
+import_stockist_keuangan()
+
+# mongoexport --collection=stockists --db=nasa_import --type=csv --out=2024/result/stockists.csv --fields=code,name,address,address2,phone,area,order,is_active,email,bank_name,bank_branch_name,bank_account_name,bank_account_number "mongodb+srv://ibrahimalanshor:65cZx8wS7s0kvjrt@cluster0.bw1af.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
