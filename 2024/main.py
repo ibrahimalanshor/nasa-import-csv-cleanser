9f3d94544a508726c8d2a075bd3e77ca8bfc2a92
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-02-28 07:10:18 +0700

    bonus

--- 2024/main.py
+++ 2024/main.py
@@ -42,7 +42,7 @@ def import_stockist_penjualan():
             bar.update(1000)
 
 def import_stockist_keuangan():
-    print('Importing raw stockist penjualan')
+    print('Importing raw stockist keuangan')
 
     filepath = os.path.join(os.path.dirname(__file__), './files/stockist-keuangan.csv')
     chunks = pd.read_csv(filepath, chunksize=1000, encoding='latin1', keep_default_na=False)
@@ -71,8 +71,49 @@ def import_stockist_keuangan():
 
             bar.update(1000)
 
+def import_stockist_bonus():
+    print('Importing raw stockist bonus')
+
+    filepath = os.path.join(os.path.dirname(__file__), './files/stockist-bonus.csv')
+    chunks = pd.read_csv(filepath, chunksize=1000, encoding='latin1', keep_default_na=False)
+    filesize = len(pd.read_csv(filepath, encoding='latin1').index)
+
+    with progressbar.ProgressBar(max_value=filesize) as bar:
+        for df in chunks:
+            payload = [
+                UpdateOne(
+                    {'code': row['KODEST']},
+                    {'$set': {
+                        'code': row['KODEST'],
+                        'member_code': row['KDEDST'],
+                        'name': row['NMAST'],
+                        'city': row['KTAST'],
+                        'phone': row['TLPST'],
+                        'mobile_number': row['HP'],
+                        'period': row['TGLMASUK'],
+                        'pin': row['PIN'],
+                        'email': row['EMAIL'],
+                        'upline_code': row['KODEUPL'],
+                        'upline_name': row['NMAUPL'],
+                        'area': row['KAREA'],
+                        'address': row['ALMST'],
+                        'bank_account_number': row['NMRREK'],
+                        'bank_name': row['NMABANK'],
+                        'bank_branch_name': row['CBNBANK'],
+                        'order': row['URUT'],
+                    }},
+                    upsert=True
+                )
+                for _, row in df.iterrows()
+            ]
+
+            db.stockists.bulk_write(payload)
+
+            bar.update(1000)
+
 
 import_stockist_penjualan()
 import_stockist_keuangan()
+import_stockist_bonus()
 
 # mongoexport --collection=stockists --db=nasa_import --type=csv --out=2024/result/stockists.csv --fields=code,name,address,address2,phone,area,order,is_active,email,bank_name,bank_branch_name,bank_account_name,bank_account_number "mongodb+srv://ibrahimalanshor:65cZx8wS7s0kvjrt@cluster0.bw1af.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
